ae74715c335d9dd5a10347a3cc97a50965b4773a
hayk9704 <hayk.gevorgyan.2013@gmail.com>
2026-05-08 17:26:23 +0300

    skeleton changes finalized :wq

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,5 +1,11 @@
-# Checks the version of the twfeiw
+# To run the test do this in your (.venv) environment:
+# python -m pytest
 
+# This test_import is usually used for import level standard checks.
+# Afterwards I have to add other files to check other staff
+
+# the most basic check (smoke test). Can python import the package?
+# Does it have the correct version?
 def test_import_twfeiw():
     import twfeiw
 
